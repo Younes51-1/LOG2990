01eaa3ee5055ec06e627a9186d466831ee5da7b9
Younes51-1 <77343582+Younes51-1@users.noreply.github.com>
2023-01-24 11:04:30 -0500

    fixing infinite loop error

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { GameCardComponent } from './components/game-card/game-card.component';
 
 /**
@@ -32,7 +33,7 @@ import { GameCardComponent } from './components/game-card/game-card.component';
         FormsModule,
         HttpClientModule,
         SlickCarouselModule,
-        GameCardComponent,
+        MatExpansionModule,
     ],
 })
 export class AppModule {}
