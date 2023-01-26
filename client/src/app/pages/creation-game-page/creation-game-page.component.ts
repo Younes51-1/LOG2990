c4e499394117a7ea9d2641319c227b9e504ef1fb
imeneghazi <90001259+imeneghazi@users.noreply.github.com>
2023-01-26 11:35:52 -0500

    récupération + affichage des images

--- client/src/app/pages/creation-game-page/creation-game-page.component.ts
+++ client/src/app/pages/creation-game-page/creation-game-page.component.ts
@@ -8,14 +8,15 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 export class CreationGamePageComponent implements AfterViewInit {
     @ViewChild('image1', { static: false }) image1: ElementRef;
     @ViewChild('image2', { static: false }) image2: ElementRef;
-    @ViewChild('deuxImages', { static: false }) deuxImages: ElementRef;
+    @ViewChild('images1et2', { static: false }) images1et2: ElementRef;
     @ViewChild('canvas1', { static: false }) canvas1: ElementRef<HTMLCanvasElement>;
     @ViewChild('canvas2', { static: false }) canvas2: ElementRef<HTMLCanvasElement>;
-    context1: CanvasRenderingContext2D | null;
-    context2: CanvasRenderingContext2D | null;
+    context1: CanvasRenderingContext2D;
+    context2: CanvasRenderingContext2D;
     width: number;
     height: number;
-    background = 'white';
+    background1: string;
+    background2: string;
 
     constructor() {
         this.width = 640;
@@ -23,18 +24,37 @@ export class CreationGamePageComponent implements AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        this.context1 = this.canvas1.nativeElement.getContext('2d');
-        if (this.context1) {
-            const background = new Image();
-            background.src = 'https://fjolt.com/images/misc/202203281.png';
-            background.onload = () => {
-                if (this.context1) this.context1.drawImage(background, 0, 0, this.width, this.height);
-            };
-        }
-        this.context2 = this.canvas2.nativeElement.getContext('2d');
+        const context1Init = this.canvas1.nativeElement.getContext('2d');
+        if (context1Init) this.context1 = context1Init;
+        const context2Init = this.canvas2.nativeElement.getContext('2d');
+        if (context2Init) this.context2 = context2Init;
     }
-    updateImageDisplay() {
-        //
+    updateImageDisplay(event: Event, input: HTMLInputElement): void {
+        if (event) {
+            const file = (event.target as HTMLInputElement).files;
+            if (file) {
+                if (input === this.image1.nativeElement) {
+                    this.background1 = URL.createObjectURL(file[0]);
+                    this.updateContext(this.context1, this.background1);
+                }
+                if (input === this.image2.nativeElement) {
+                    this.background2 = URL.createObjectURL(file[0]);
+                    this.updateContext(this.context2, this.background2);
+                }
+                if (input === this.images1et2.nativeElement) {
+                    this.background1 = this.background2 = URL.createObjectURL(file[0]);
+                    this.updateContext(this.context1, this.background1);
+                    this.updateContext(this.context2, this.background2);
+                }
+            }
+        }
+    }
+    updateContext(context: CanvasRenderingContext2D, background: string): void {
+        const image = new Image();
+        image.src = background;
+        image.onload = () => {
+            context.drawImage(image, 0, 0, this.width, this.height);
+        };
     }
     verifyImageContent(): void {
         const img1Src: string = this.image1.nativeElement.value;
@@ -51,12 +71,8 @@ export class CreationGamePageComponent implements AfterViewInit {
     reset(): void {
         this.image1.nativeElement.value = null;
         this.image2.nativeElement.value = null;
-        this.deuxImages.nativeElement.value = null;
-        if (this.context1) {
-            this.context1.clearRect(0, 0, this.canvas1.nativeElement.width, this.canvas2.nativeElement.height);
-        }
-        if (this.context2) {
-            this.context2.clearRect(0, 0, this.canvas1.nativeElement.width, this.canvas2.nativeElement.height);
-        }
+        this.images1et2.nativeElement.value = null;
+        this.context1.clearRect(0, 0, this.canvas1.nativeElement.width, this.canvas2.nativeElement.height);
+        this.context2.clearRect(0, 0, this.canvas1.nativeElement.width, this.canvas2.nativeElement.height);
     }
 }
