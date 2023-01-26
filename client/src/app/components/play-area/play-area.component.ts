3f110f67b3ed1c279f89571d9472b1f19600df4e
Coralie Brodeur <coralie.brodeur@polymtl.ca>
2023-01-26 18:39:09 -0500

    début vue et image display

--- client/src/app/components/play-area/play-area.component.ts
+++ client/src/app/components/play-area/play-area.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Vec2 } from '@app/interfaces/vec2';
-import { DrawService } from '@app/services/draw.service';
+// import { DrawService } from '@app/services/draw.service';
 
 // TODO : Avoir un fichier séparé pour les constantes!
 export const DEFAULT_WIDTH = 500;
@@ -21,13 +21,17 @@ export enum MouseButton {
     styleUrls: ['./play-area.component.scss'],
 })
 export class PlayAreaComponent implements AfterViewInit {
-    @ViewChild('gridCanvas', { static: false }) private canvas!: ElementRef<HTMLCanvasElement>;
+    // @ViewChild('gridCanvas', { static: false }) private canvas!: ElementRef<HTMLCanvasElement>;
+    @ViewChild('canvas1', { static: false }) canvas1: ElementRef<HTMLCanvasElement>;
+    @ViewChild('canvas2', { static: false }) canvas2: ElementRef<HTMLCanvasElement>;
+    context1: CanvasRenderingContext2D;
+    context2: CanvasRenderingContext2D;
 
     mousePosition: Vec2 = { x: 0, y: 0 };
     buttonPressed = '';
 
     private canvasSize = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
-    constructor(private readonly drawService: DrawService) {}
+    // constructor(private readonly drawService: DrawService) {}
 
     get width(): number {
         return this.canvasSize.x;
@@ -43,10 +47,23 @@ export class PlayAreaComponent implements AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        this.drawService.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        this.drawService.drawGrid();
-        this.drawService.drawWord('Différence');
-        this.canvas.nativeElement.focus();
+        const context1 = this.canvas1.nativeElement.getContext('2d');
+        if (context1) this.context1 = context1;
+        const context2 = this.canvas2.nativeElement.getContext('2d');
+        if (context2) this.context2 = context2;
+        // this.drawService.drawGrid();
+        // this.drawService.drawWord('Différence');
+        // this.canvas.nativeElement.focus();
+        const image = new Image();
+        image.src = '../../../assets/card.png';
+        image.onload = () => {
+            if (context1 != null) {
+                context1.drawImage(image, 0, 0, this.width, this.height);
+            }
+            if (context2 != null) {
+                context2.drawImage(image, 0, 0, this.width, this.height);
+            }
+        };
     }
 
     // TODO : déplacer ceci dans un service de gestion de la souris!
