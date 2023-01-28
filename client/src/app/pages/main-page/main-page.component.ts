d86b328bbf99cb634b77e596a88ca412d3ccbbf1
Coralie Brodeur <coralie.brodeur@polymtl.ca>
2023-01-28 00:28:45 -0500

    design done

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
-    readonly title: string = 'jeu des différences !';
+    readonly title: string = 'Jeu des différences';
     readonly teamName: string = '204';
     readonly teamMembers: string[] = [
         'Coralie Brodeur',
