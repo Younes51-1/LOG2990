2bff1c53afd11a924d9925f1eacff329e02d6adb
Thibault D <48220581+joysecc@users.noreply.github.com>
2023-01-31 12:35:39 -0500

    tests for Modal-Dialog

--- client/src/app/components/modal-dialog/modal-dialog.component.spec.ts
+++ client/src/app/components/modal-dialog/modal-dialog.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { ModalDialogComponent } from './modal-dialog.component';
 
 describe('ModalDialogComponent', () => {
@@ -22,5 +23,24 @@ describe('ModalDialogComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should draw canvas', () => {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        const context = CanvasTestHelper.createCanvas(640, 480).getContext('2d') as CanvasRenderingContext2D;
+        component.context = context;
+        const drawImageSpy = spyOn(component.context, 'drawImage').and.stub();
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        context.clearRect(0, 0, 640, 480);
+        component.data.imageUrl = context.canvas.toDataURL();
+        component.ngAfterViewInit();
+        expect(drawImageSpy).not.toHaveBeenCalled();
+    });
+
+    it('should show the right amount of differences', () => {
+        component.data.nbDifferences = 5;
+        fixture.detectChanges();
+        const differences = fixture.nativeElement.querySelector('p');
+        expect(differences.textContent).toContain('5');
     });
 });
--- client/src/app/pages/config-page/config-page.component.spec.ts
+++ client/src/app/pages/config-page/config-page.component.spec.ts
@@ -1,24 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConfigPageComponent } from '@app/pages/config-page/config-page.component';
-import { of } from 'rxjs';
-
-export class MatDialogMock {
-    open() {
-        return {
-            afterClosed: () => of({}),
-        };
-    }
-}
 
 describe('ConfigPageComponent', () => {
     let component: ConfigPageComponent;
@@ -27,24 +8,7 @@ describe('ConfigPageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ConfigPageComponent],
-            imports: [
-                BrowserAnimationsModule,
-                FormsModule,
-                MatButtonModule,
-                MatCardModule,
-                MatDialogModule,
-                MatExpansionModule,
-                MatIconModule,
-                MatRadioModule,
-                MatToolbarModule,
-                MatTooltipModule,
-            ],
-            providers: [
-                {
-                    provide: MatDialog,
-                    useClass: MatDialogMock,
-                },
-            ],
+            providers: [],
         }).compileComponents();
     });
 
@@ -56,12 +20,5 @@ describe('ConfigPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('onLikeLoremTheme() should open a dialog', () => {
-        // eslint-disable-next-line -- matDialog is private and we need access for the test
-        const spy = spyOn(component['matDialog'], 'open');
-        component.onLikeTheme();
-        expect(spy).toHaveBeenCalled();
     });
 });
