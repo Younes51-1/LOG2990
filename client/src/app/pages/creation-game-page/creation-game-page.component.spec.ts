e3cb5db9ab54aa6c597f3739034c07368cce27e9
imeneghazi <90001259+imeneghazi@users.noreply.github.com>
2023-01-31 15:34:45 -0500

    tests on radio buttons and reset()

--- client/src/app/pages/creation-game-page/creation-game-page.component.spec.ts
+++ client/src/app/pages/creation-game-page/creation-game-page.component.spec.ts
@@ -1,8 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { CreationGamePageComponent } from './creation-game-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { CreationGamePageComponent } from './creation-game-page.component';
 
 describe('CreationGamePageComponent', () => {
     let component: CreationGamePageComponent;
@@ -31,5 +31,33 @@ describe('CreationGamePageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('radius must be updated when a radio button is selected', () => {
+        const radiuses = [0, 3, 9, 15];
+        const spy = spyOn(component, 'updateRadius').and.callThrough();
+        for (const radius of radiuses) {
+            const inputRayon = fixture.debugElement.nativeElement.querySelector('#rayon' + radius);
+            inputRayon.click();
+            expect(spy).toHaveBeenCalled();
+            expect(component.radius).toBe(radius);
+        }
+    });
+    it('by default, radio button 3 must be selected', () => {
+        const inputRayon3 = fixture.debugElement.nativeElement.querySelector('#rayon3');
+        expect(inputRayon3.checked).toBeTruthy();
+    });
+
+    it('reset should leave the canvas blank', () => {
+        const image = new Image();
+        image.src = 'image_2_diff.bmp';
+        image.onload = () => {
+            component.context1.drawImage(image, 0, 0, component.width, component.height);
+        };
+        const spyReset = spyOn(component, 'reset').and.callThrough();
+        const spyClearRect = spyOn(component.context1, 'clearRect').and.callThrough();
+        component.reset();
+        expect(spyReset).toHaveBeenCalled();
+        expect(spyClearRect).toHaveBeenCalled();
     });
 });
