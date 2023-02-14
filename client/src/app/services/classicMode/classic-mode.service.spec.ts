c4e7a1df6b553bebf5f93446028bb501197b0dc1
Thibault D <48220581+joysecc@users.noreply.github.com>
2023-02-14 17:05:59 -0500

    added handleSocket tests

--- client/src/app/services/classicMode/classic-mode.service.spec.ts
+++ client/src/app/services/classicMode/classic-mode.service.spec.ts
@@ -1,26 +1,43 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { DifferenceTry } from '@app/interfaces/difference-try';
 import { GameData } from '@app/interfaces/game-data';
-import { of } from 'rxjs';
 import { CommunicationService } from '@app/services/communicationService/communication.service';
 import { CommunicationSocketService } from '@app/services/communicationSocket/communication-socket.service';
+import { of } from 'rxjs';
+import { Socket } from 'socket.io-client';
 import { ClassicModeService } from './classic-mode.service';
 
 const differenceMatrix: number[][] = [[]];
 const gameForm = { name: '', nbDifference: 0, image1url: '', image2url: '', difficulte: '', soloBestTimes: [], vsBestTimes: [] };
 const gameData: GameData = { gameForm, differenceMatrix };
+const gameRoom = { userGame: { gameData, nbDifferenceFound: 0, timer: 0, username: 'Test' }, roomId: 'fakeId' };
+const differenceTry: DifferenceTry = { validated: true, differencePos: { x: 0, y: 0 } };
+
+class SocketClientServiceMock extends CommunicationSocketService {
+    override connect() {
+        return;
+    }
+}
 
 describe('ClassicModeService', () => {
     let service: ClassicModeService;
     let communicationSocketService: CommunicationSocketService;
     let communicationService: CommunicationService;
+    let socketServiceMock: SocketClientServiceMock;
+    let socketHelper: SocketTestHelper;
 
     beforeEach(() => {
+        socketHelper = new SocketTestHelper();
+        socketServiceMock = new SocketClientServiceMock();
+        socketServiceMock.socket = socketHelper as unknown as Socket;
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
-            providers: [CommunicationSocketService, CommunicationService],
+            providers: [CommunicationSocketService, CommunicationService, { provide: CommunicationSocketService, useValue: socketServiceMock }],
         });
         service = TestBed.inject(ClassicModeService);
+        service.handleSocket();
     });
 
     it('should be created', () => {
@@ -120,12 +137,41 @@ describe('ClassicModeService', () => {
         expect(spy2).toHaveBeenCalled();
     });
 
-    it('should handle the socket', () => {
-        communicationSocketService = TestBed.inject(CommunicationSocketService);
-        const spy = spyOn(communicationSocketService, 'on').and.callFake(() => {
+    it('should handle on waiting message', () => {
+        const spy = spyOn(service, 'startGame').and.callFake(() => {
             return;
         });
-        service.handleSocket();
+        socketHelper.peerSideEmit('waiting');
         expect(spy).toHaveBeenCalled();
+    });
+
+    it('should handle on started message', () => {
+        service.gameRoom = gameRoom;
+        service.gameRoom.roomId = '';
+        socketHelper.peerSideEmit('started', gameRoom.roomId);
+        expect(service.gameRoom.roomId).toEqual(gameRoom.roomId);
+    });
+
+    it('should handle on validated message', () => {
+        service.gameRoom = gameRoom;
+        socketHelper.peerSideEmit('validated', differenceTry);
+        expect(service.gameRoom.userGame.nbDifferenceFound).toEqual(1);
+        expect(service.serverValidateResponse$).toBeTruthy();
+    });
+
+    it('should handle on GameFinished message', () => {
+        service.gameRoom = gameRoom;
+        const spy = spyOn(socketServiceMock, 'disconnect').and.callFake(() => {
+            return;
+        });
+        socketHelper.peerSideEmit('GameFinished', gameData);
+        expect(service.gameFinished$).toBeTruthy();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should handle on timer message', () => {
+        service.gameRoom = gameRoom;
+        socketHelper.peerSideEmit('timer', 1);
+        expect(service.gameRoom.userGame.timer).toEqual(1);
     });
 });
