288d75b932d050b6487f77fdc27daec29c3534ee
Younes51-1 <77343582+Younes51-1@users.noreply.github.com>
2023-03-07 22:50:41 -0500

    mocked services in gameCard

--- client/src/app/components/game-card/game-card.component.spec.ts
+++ client/src/app/components/game-card/game-card.component.spec.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameCardComponent } from '@app/components/game-card/game-card.component';
 import { PageKeys, options } from 'src/assets/variables/game-card-options';
@@ -5,20 +6,36 @@ import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ClassicModeService } from '@app/services/classicMode/classic-mode.service';
+import { CommunicationSocketService } from '@app/services/communicationSocket/communication-socket.service';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { Socket } from 'socket.io-client';
 
 @NgModule({
     imports: [HttpClientModule],
 })
 export class DynamicTestModule {}
 
+class SocketClientServiceMock extends CommunicationSocketService {
+    override connect() {
+        return;
+    }
+}
+
 describe('GameCardComponent', () => {
     let component: GameCardComponent;
     let fixture: ComponentFixture<GameCardComponent>;
+    let socketServiceMock: SocketClientServiceMock;
+    let socketHelper: SocketTestHelper;
 
     beforeEach(async () => {
+        socketHelper = new SocketTestHelper();
+        socketServiceMock = new SocketClientServiceMock();
+        socketServiceMock.socket = socketHelper as unknown as Socket;
         await TestBed.configureTestingModule({
             declarations: [GameCardComponent],
             imports: [AppRoutingModule, DynamicTestModule, RouterTestingModule],
+            providers: [ClassicModeService, CommunicationSocketService, { provide: CommunicationSocketService, useValue: socketServiceMock }],
         }).compileComponents();
     });
     beforeEach(() => {
@@ -70,10 +87,10 @@ describe('GameCardComponent', () => {
         expect(component.slide.vsBestTimes.length).toEqual(3);
     });
 
-    // it('should have play button for solo mode', () => {
-    //     const btn1 = fixture.debugElement.nativeElement.getElementsByTagName('button')[0];
-    //     expect(btn1).not.toBeUndefined();
-    // });
+    it('should have play button for solo mode', () => {
+        const btn1 = fixture.debugElement.nativeElement.getElementsByTagName('button')[0];
+        expect(btn1).not.toBeUndefined();
+    });
 
     it('should have create/join button for 1v1 mode', () => {
         const btn2 = fixture.debugElement.nativeElement.getElementsByTagName('button')[1];
